68f9880ae4df8863efaf747d43f560194440cf47
Raphael Da Silva <raphds@yahoo.fr>
2023-06-24 12:30:12 +0200

    Nouvelle version bataille navale

---
+++ cmake-build-debug/CMakeFiles/V1_Bataille_Navale.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/V1_Bataille_Navale.dir/main.c.o"
+  "CMakeFiles/V1_Bataille_Navale.dir/main.c.o.d"
+  "V1_Bataille_Navale"
+  "V1_Bataille_Navale.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/V1_Bataille_Navale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/V1_Bataille_Navale.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for V1_Bataille_Navale.
+# This may be replaced when dependencies are built.
